c7c069d379d62fa913c309ce53eebd1ec5826388
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2023-02-12 01:59:14 -0500

    springboot 3.0

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
 
     @RequestMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
 }
